6964733ebd0c8db19345c11fac033f9e97346d10
thays <thaysdasilvamariano@gmail.com>
2025-01-13 15:35:51 -0300

    Index na raiz

--- jogo/script.js
+++ jogo/script.js
@@ -1,89 +1,89 @@
 
-//objeto
-var pontuacao ={
-  vitorias: 0,
-  derrotas: 0,
-  empates: 0,
-};
+// //objeto
+// var pontuacao ={
+//   vitorias: 0,
+//   derrotas: 0,
+//   empates: 0,
+// };
 
 
 
-//funcoes
+// //funcoes
 
-function escolherMovimentoComputador() {
+// function escolherMovimentoComputador() {
 
-const numeroAleatorio3 = Math.random();
+// const numeroAleatorio3 = Math.random();
 
-let movimentoComputador = ' ';
-if (numeroAleatorio3 >= 0 && numeroAleatorio3 < 1 / 3) {
-  movimentoComputador = 'pedra';
-}
-else if (numeroAleatorio3 >= 1 / 3 && numeroAleatorio3 < 2 / 3) {
-  movimentoComputador = 'papel';
-}
-else {
-  movimentoComputador = 'tesoura';
-}
+// let movimentoComputador = ' ';
+// if (numeroAleatorio3 >= 0 && numeroAleatorio3 < 1 / 3) {
+//   movimentoComputador = 'pedra';
+// }
+// else if (numeroAleatorio3 >= 1 / 3 && numeroAleatorio3 < 2 / 3) {
+//   movimentoComputador = 'papel';
+// }
+// else {
+//   movimentoComputador = 'tesoura';
+// }
 
-return movimentoComputador;
-}
+// return movimentoComputador;
+// }
 
-function resultadoJogo(movimentoJogador) {
+// function resultadoJogo(movimentoJogador) {
 
-movimentoComputador = escolherMovimentoComputador();
+// movimentoComputador = escolherMovimentoComputador();
 
-let resultado = ' ';
-if (movimentoJogador === 'tesoura') {
-  if (movimentoComputador === 'pedra') {
-    resultado = 'Voce perdeu!';
-  }
-  else if (movimentoComputador === 'papel') {
-    resultado = 'Voce ganhou!'
-  }
-  else {
-    resultado = 'Empate!'
+// let resultado = ' ';
+// if (movimentoJogador === 'tesoura') {
+//   if (movimentoComputador === 'pedra') {
+//     resultado = 'Voce perdeu!';
+//   }
+//   else if (movimentoComputador === 'papel') {
+//     resultado = 'Voce ganhou!'
+//   }
+//   else {
+//     resultado = 'Empate!'
 
-  }
-}
+//   }
+// }
 
-else if (movimentoJogador === 'pedra') {
-  if (movimentoComputador === 'pedra') {
-    resultado = 'Empate!';
+// else if (movimentoJogador === 'pedra') {
+//   if (movimentoComputador === 'pedra') {
+//     resultado = 'Empate!';
     
-  }
-  else if (movimentoComputador === 'papel') {
-    resultado = 'Voce perdeu!'
+//   }
+//   else if (movimentoComputador === 'papel') {
+//     resultado = 'Voce perdeu!'
     
-  }
-  else {
-    resultado = 'Voce ganhou!'
+//   }
+//   else {
+//     resultado = 'Voce ganhou!'
     
-  }
-}
+//   }
+// }
 
-else {
-  if (movimentoComputador === 'pedra') {
-    resultado = 'Voce ganhou!';
+// else {
+//   if (movimentoComputador === 'pedra') {
+//     resultado = 'Voce ganhou!';
     
-  }
-  else if (movimentoComputador === 'papel') {
-    resultado = 'Empate!'
+//   }
+//   else if (movimentoComputador === 'papel') {
+//     resultado = 'Empate!'
     
-  }
-  else {
-    resultado = 'Voce perdeu!'
+//   }
+//   else {
+//     resultado = 'Voce perdeu!'
     
-  }
-}
-if (resultado === 'Voce ganhou!'){
-  pontuacao.vitorias++
-}else if (resultado === 'Voce perdeu!'){
-  pontuacao.derrotas++
-}else{
-  pontuacao.empates++
-}
+//   }
+// }
+// if (resultado === 'Voce ganhou!'){
+//   pontuacao.vitorias++
+// }else if (resultado === 'Voce perdeu!'){
+//   pontuacao.derrotas++
+// }else{
+//   pontuacao.empates++
+// }
 
-return alert(`Voce escolheu ${movimentoJogador}. O computador escolheu ${movimentoComputador}. ${resultado}
-  v ${pontuacao.vitorias}, d ${pontuacao.derrotas}, e ${pontuacao.empates}`);
-}
+// return alert(`Voce escolheu ${movimentoJogador}. O computador escolheu ${movimentoComputador}. ${resultado}
+//   vitorias: ${pontuacao.vitorias}, derrotas: ${pontuacao.derrotas}, empates: ${pontuacao.empates}`);
+// }
 
